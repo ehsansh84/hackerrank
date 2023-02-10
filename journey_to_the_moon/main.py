735f6a78bbc65882230ef0061077db02e31801ac
ehsan <ehsan.shirzadi@gmail.com>
2023-02-10 11:22:22 +0330

    updated: 9 test passed / 7 failed

--- journey_to_the_moon/main.py
+++ journey_to_the_moon/main.py
@@ -14,7 +14,7 @@ import sys
 #  1. INTEGER n
 #  2. 2D_INTEGER_ARRAY astronaut
 #
-
+# @profile
 def get_astronaut_groups(astronaut):
     astronaut_groups = []
     for i in astronaut:
@@ -25,44 +25,78 @@ def get_astronaut_groups(astronaut):
                     temp = temp | j
         if temp not in astronaut_groups:
             astronaut_groups.append(temp)
+    print(astronaut_groups)
     return astronaut_groups
 
-
+# @profile
 def journeyToMoon(n, astronaut):
-    from itertools import product
-    l = []
     astronaut_groups = get_astronaut_groups(astronaut)
+    from itertools import permutations
+    astronaut_groups_of_two = []
+    for astronaut_group in astronaut_groups:
+        for pair in permutations(astronaut_group, 2):
 
-    all_possible_pairs = product(range(n), range(n))
-    valid_pairs = []
-    for pair in all_possible_pairs:
-        pair_set = set(pair)
-        if pair[0] != pair[1]:
-            sub = False
-            for ag in astronaut_groups:
-                if pair_set.issubset(ag):
-                    sub = True
-                    break
-            if not sub:
-                if pair_set not in valid_pairs:
-                    valid_pairs.append(pair_set)
-    return len(valid_pairs)
+            if not set(pair) in astronaut_groups_of_two:
+                # print(pair)
+                astronaut_groups_of_two.append(set(pair))
+    print(f'astronaut_groups_of_two {astronaut_groups_of_two}')
+    print(astronaut_groups)
+    total_probs = n * (n-1) / 2
+
+    print(total_probs)
+    print(len(astronaut))
+    return int(total_probs - len(astronaut_groups_of_two))
+    # return int(total_probs - len(astronaut))
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    first_multiple_input = input().rstrip().split()
+    with open('data2') as f:
+        first_multiple_input = f.readline().rstrip().split()
+        n = int(first_multiple_input[0])
+        p = int(first_multiple_input[1])
+        astronaut = []
+        for _ in range(p):
+            astronaut.append(set(list(map(int, f.readline().rstrip().split()))))
+        result = journeyToMoon(n, astronaut)
+        print(result)
 
-    n = int(first_multiple_input[0])
 
-    p = int(first_multiple_input[1])
 
-    astronaut = []
 
-    for _ in range(p):
-        astronaut.append(set(list(map(int, input().rstrip().split()))))
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    # first_multiple_input = input().rstrip().split()
+    # n = int(first_multiple_input[0])
+    # p = int(first_multiple_input[1])
+    # astronaut = []
+    # for _ in range(p):
+    #     astronaut.append(set(list(map(int, input().rstrip().split()))))
+    # result = journeyToMoon(n, astronaut)
+    # print(result)
+    # fptr.write(str(result) + '\n')
+    # fptr.close()
 
-    result = journeyToMoon(n, astronaut)
 
-    fptr.write(str(result) + '\n')
 
-    fptr.close()
+
+
+
+
+
+# def journeyToMoon(n, astronaut):
+#     from itertools import product
+#     l = []
+#     astronaut_groups = get_astronaut_groups(astronaut)
+#
+#     all_possible_pairs = product(range(n), range(n))
+#     valid_pairs = []
+#     for pair in all_possible_pairs:
+#         pair_set = set(pair)
+#         if pair[0] != pair[1]:
+#             sub = False
+#             for ag in astronaut_groups:
+#                 if pair_set.issubset(ag):
+#                     sub = True
+#                     break
+#             if not sub:
+#                 if pair_set not in valid_pairs:
+#                     valid_pairs.append(pair_set)
+#     return len(valid_pairs)
