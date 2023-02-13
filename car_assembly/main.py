0303a276eacddff707c221eae7a790d0cc6c5b86
ehsan <ehsan.shirzadi@gmail.com>
2023-02-13 20:49:42 +0330

    updated: test 11 passed

--- car_assembly/main.py
+++ car_assembly/main.py
@@ -8,9 +8,8 @@ class AssemblyOperation(NamedTuple):
     dependencies: Tuple[str, ...]
 
 def minimum_assembly_time(ops: List[AssemblyOperation]) -> int:
-    # Enter your code here: return an interger indicating the answer to the problem
     tasks_dic = {task.name: {'duration': task.duration, 'dependencies': task.dependencies} for task in ops }
-
+    # print(tasks_dic)
     def get_dependencies_times(task: AssemblyOperation):
         dep_times = []
         if not task.dependencies:
@@ -18,27 +17,28 @@ def minimum_assembly_time(ops: List[AssemblyOperation]) -> int:
         if 'dep_duration' in tasks_dic[task.name]:
             return tasks_dic[task.name]['dep_duration']
         for dep in task.dependencies:
-            temp = AssemblyOperation(dep,int(tasks_dic[dep]['duration']),tasks_dic[dep]['dependencies'])
-            # print(f'temp.name{temp.name}')
-            dep_time = tasks_dic[dep]['duration'] + get_dependencies_times(temp)
-            dep_times.append(dep_time)
+            try:
+                temp = AssemblyOperation(dep, int(tasks_dic[dep]['duration']), tasks_dic[dep]['dependencies'])
+                dep_time = tasks_dic[dep]['duration'] + get_dependencies_times(temp)
+                dep_times.append(dep_time)
+            except KeyError:
+                return -1
         t = max(dep_times)
-        # print(tasks_dic)
-        # print(task.name)
-        # print(tasks_dic[task.name])
         tasks_dic[task.name]['dep_duration'] = t
         return t
     all_task_times = []
     for item in ops:
-        all_task_times.append(item.duration + get_dependencies_times(item))
-    # print(tasks_dic)
+        dep_time = get_dependencies_times(item)
+        if dep_time == -1:
+            return -1
+        all_task_times.append(item.duration + dep_time)
     return max(all_task_times)
 
 
 def main():
     ops = []
     # for line in sys.stdin:
-    with open('data-tc12') as f:
+    with open('data-tc11') as f:
         for line in f.readlines():
             # print(line)
             name, duration, *dependencies = line.rstrip().split()
