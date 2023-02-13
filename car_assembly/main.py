747a78f678c7c577f5404c2a30604d04bfdcda35
ehsan <ehsan.shirzadi@gmail.com>
2023-02-13 20:37:31 +0330

    updated: test 12 passed

--- car_assembly/main.py
+++ car_assembly/main.py
@@ -15,21 +15,30 @@ def minimum_assembly_time(ops: List[AssemblyOperation]) -> int:
         dep_times = []
         if not task.dependencies:
             return 0
+        if 'dep_duration' in tasks_dic[task.name]:
+            return tasks_dic[task.name]['dep_duration']
         for dep in task.dependencies:
-            temp = AssemblyOperation(tasks_dic[dep],int(tasks_dic[dep]['duration']),tasks_dic[dep]['dependencies'])
+            temp = AssemblyOperation(dep,int(tasks_dic[dep]['duration']),tasks_dic[dep]['dependencies'])
+            # print(f'temp.name{temp.name}')
             dep_time = tasks_dic[dep]['duration'] + get_dependencies_times(temp)
             dep_times.append(dep_time)
-        return max(dep_times)
+        t = max(dep_times)
+        # print(tasks_dic)
+        # print(task.name)
+        # print(tasks_dic[task.name])
+        tasks_dic[task.name]['dep_duration'] = t
+        return t
     all_task_times = []
     for item in ops:
         all_task_times.append(item.duration + get_dependencies_times(item))
+    # print(tasks_dic)
     return max(all_task_times)
 
 
 def main():
     ops = []
     # for line in sys.stdin:
-    with open('data') as f:
+    with open('data-tc12') as f:
         for line in f.readlines():
             # print(line)
             name, duration, *dependencies = line.rstrip().split()
